5a1bb7f4103364c010ecdbecc92ba9610793bf95
DevPatel1023 <devp4143@gmail.com>
2024-03-03 21:39:07 +0530

    commit

--- server/controllers/maincontroller.js
+++ server/controllers/maincontroller.js
@@ -5,11 +5,15 @@ HOMEPAGE
 
 exports.homePage = async (req ,res) => {
     const locals = {
-        title:"Node-Js APP",
-        description : "This is an Node-Js Notes App"
+        title: "Node-Js APP",
+        description: "This is an Node-Js Notes App"
     }
-    res.render('index',locals);
+    res.render('index', {
+        locals,
+        layout: '../views/layouts/front-page' // Specify the layout name without the path
+    });
 }
+
 
 /*
 GET
