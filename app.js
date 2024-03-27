d5906fecf7baedf4da45fc0550a6462507830277
DevPatel1023 <devp4143@gmail.com>
2024-03-27 21:59:05 +0530

    commit

--- app.js
+++ app.js
@@ -1,60 +1,58 @@
-require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require("method-override");
 const connectDB = require('./server/config/db');
-const session = require('express-session');//for user stayes logged in
-const passport = require('passport'); //authentication
-const MongoStore = require('connect-mongo');
-const authRoutes = require('./server/routes/auth');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 
-// Session config
 app.use(session({
-    secret: "keyboard cat",
+    secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // 24 hours
-    }
-}));
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongoUrl: process.env.MONGODB_URI,
+      // Additional options if needed
+    }),
+  }));
 
-// Passport config
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
-
-// Middlewares function that parse the request bodies of incoming requests
-// 'unencoded' and 'json' are the two options for the type of data that can be sent in the request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
+// Conntect to Database
+connectDB();  
 
-// Connect to database
-connectDB();
-
-//Static files
+// Static Files
 app.use(express.static('public'));
 
-
-//Template engine
+// Templating Engine
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-//Routes
+
+
+// Routes
 app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
-//Handle 404
-app.get('*',(req, res) => {
-    res.status(404).render('404');
-});
+// Handle 404
+app.get('*', function(req, res) {
+  //res.status(404).send('404 Page Not Found.')
+  res.status(404).render('404');
+})
 
-// listen on port
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,30 @@
 const moongoose = require('mongoose');
 const Schema = moongoose.Schema;
 const UserSchema = new Schema({
-    
+    googleId : {
+        type: String,
+        required: true
+    },
+    displayName : {
+        type: String,
+        required: true
+    },
+    firstName : {
+        type: String,
+        required: true
+    },
+    lastName : {
+        type: String,
+        required: true
+    },
+    profileImage : {
+        type: String,
+        required: true
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now
+    },
 });
 
 module.exports = moongoose.model('User', UserSchema);
