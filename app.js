2360450e4705afdb552f8629fcfdd6e0c733af86
DevPatel1023 <devp4143@gmail.com>
2024-03-29 12:01:52 +0530

    commit

--- app.js
+++ app.js
@@ -1,58 +1,26 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
+require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const methodOverride = require("method-override");
-const connectDB = require('./server/config/db');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = 5000 || process.env.PORT  ;
 
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    store: new MongoStore({
-      mongoUrl: process.env.MONGODB_URI,
-      // Additional options if needed
-    }),
-  }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(methodOverride("_method"));
-// Conntect to Database
-connectDB();  
 
-// Static Files
+//Static Files
 app.use(express.static('public'));
 
-// Templating Engine
+//Templating Engine
 app.use(expressLayouts);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set('layout','./layouts/main');//default layout
+app.set('view engine','ejs');
 
 
+app.get('/',(req,res)=>{
+    res.render('index');
+});
 
-// Routes
-app.use('/', require('./server/routes/auth'));
-app.use('/', require('./server/routes/index'));
-app.use('/', require('./server/routes/dashboard'));
-
-// Handle 404
-app.get('*', function(req, res) {
-  //res.status(404).send('404 Page Not Found.')
-  res.status(404).render('404');
-})
-
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
 });
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mainController = require('../controllers/maincontroller');
-
-//App Routes
-router.get('/', mainController.homePage);
-router.get('/about', mainController.about);
-
-module.exports = router;
